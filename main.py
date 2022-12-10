f089a883f25f2f3fab6f730d4120cf257a858f7a
Z <babucse11@gmail.com>
2022-12-11 01:28:05 +0600

    updated

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
+import os
 from requests.auth import HTTPBasicAuth
 from requests import Session
 from zeep import Client
 from zeep.transports import Transport
 from zeep.wsse.signature import Signature
-from OpenSSL import crypto
 
+
+wsdl_url = 'https://miportafoliouat.transunion.co/InformacionComercialWS/services/InformacionComercial?wsdl'
 user = "520825"
 password = "COHd6zaIf*08"
 
-session = Session()
-cert_file = 'certificate.pfx'
-# p12 = crypto.load_pkcs12(open("/path/to/cert.p12", 'rb').read(), password)
-pkcs12 = crypto.load_pkcs12(open(cert_file, 'rb').read(), password)
-cert = crypto.dump_certificate(crypto.FILETYPE_PEM, pkcs12.get_certificate())
-key = crypto.dump_privatekey(crypto.FILETYPE_PEM, pkcs12.get_privatekey())
+pfx_cert = 'D:\\utilities\\caoh91\\Certificados\\certificate.pfx'
+pfx_password = b'namtrik'
 
-with open(cert_file, 'wb') as f:
-    f.write(cert)    
-with open('key.pem', 'wb') as f:
-    f.write(key)
+from cryptography.hazmat.primitives.serialization import pkcs12
 
-session.cert = (cert_file, 'key.pem')
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
+with open(pfx_cert, "rb") as f:
+    private_key, certificate, additional_certificates = pkcs12.load_key_and_certificates(f.read(), pfx_password)
+    
+    print(certificate.not_valid_after)
 
-# signature = Signature(cert_file, cert_file)
-session.auth = HTTPBasicAuth(user, password)
-client = Client('https://miportafoliouat.transunion.co/InformacionComercialWS/services/InformacionComercial?wsdl',
-            transport=Transport(session=session)) #, wsse=[signature]
+    session = Session()
 
-request_data = {
-    'codigoInformacion': '1855',
-    'motivoConsulta': '24',
-    'numeroIdentificacion': '37685317',
-    'tipoIdentificacion': '1'
-}
+    try:
+        os.mkdir("certs")
+    except FileExistsError:
+        pass
+    with open("certs/public.pem", "wb") as f:
+        f.write(certificate.public_bytes(encoding=Encoding.PEM))
 
-request_parameter = {
-    'parametrosConsulta': request_data
-}
+    with open("certs/private.pem", "wb") as f:
+        f.write(private_key.private_bytes(encoding=Encoding.PEM,
+                                          format=PrivateFormat.TraditionalOpenSSL,
+                                          encryption_algorithm=NoEncryption()))
+                                          
+    signature = Signature("certs/private.pem", "certs/public.pem")
 
-print(client.service.consultaXml(**request_parameter))
+    session.auth = HTTPBasicAuth(user, password)
+    transport = Transport(session=session)
+    client = Client(wsdl_url, transport=transport, wsse=[signature])
+
+    request_data = {
+        'codigoInformacion': '1855',
+        'motivoConsulta': '24',
+        'numeroIdentificacion': '37685317',
+        'tipoIdentificacion': '1'
+    }
+
+    request_parameter = {
+        'parametrosConsulta': request_data
+    }
+
+    print(client.service.consultaXml(**request_parameter))
