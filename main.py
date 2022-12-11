c9b85b8514f279dc9eff59f18e31f6a99ac78bfa
Z <babucse11@gmail.com>
2022-12-11 10:22:47 +0600

    timestamp work in progress

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from requests import Session
 from zeep import Client
 from zeep.transports import Transport
 from zeep.wsse.signature import Signature
-
+from zeep.plugins import HistoryPlugin
+from lxml import etree
+import xml.etree.ElementTree as ET
+from zeep.exceptions import Fault
+from cryptography.hazmat.primitives.serialization import pkcs12
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
+from cryptography.hazmat.backends import default_backend
+from zeep.wsse import utils
+from datetime import datetime, timedelta
 
 wsdl_url = 'https://miportafoliouat.transunion.co/InformacionComercialWS/services/InformacionComercial?wsdl'
 user = "520825"
@@ -13,20 +21,52 @@ password = "COHd6zaIf*08"
 pfx_cert = 'D:\\utilities\\caoh91\\Certificados\\certificate.pfx'
 pfx_password = "namtrik".encode('utf8')# b'namtrik'
 
-from cryptography.hazmat.primitives.serialization import pkcs12
-from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
-from cryptography.hazmat.backends import default_backend
-
-class CustomSignature(object):
+class BinarySignatureTimestamp(object):
     """Sign given SOAP envelope with WSSE sig using given key and cert."""
     def __init__(self, wsse_list):
         self.wsse_list = wsse_list
 
     def apply(self, envelope, headers):
-        for wsse in self.wsse_list:
-            envelope, headers = wsse.apply(envelope, headers)
-        return envelope, headers
+    #     for wsse in self.wsse_list:
+    #         envelope, headers = wsse.apply(envelope, headers)
+    #     return envelope, headers
+        security = utils.get_security_header(envelope)
 
+        created = datetime.now()
+        expired = created + timedelta(seconds=5 * 60)
+
+        token = utils.WSSE.UsernameToken()
+        token.extend([
+            utils.WSSE.Nonce('43d74dda16a061874d9ff27f2b40e017'),
+            utils.WSSE.Created(utils.get_timestamp(created)),
+        ])
+
+        timestamp = utils.WSU('Timestamp')
+        timestamp.append(utils.WSU('Created', utils.get_timestamp(created)))
+        timestamp.append(utils.WSU('Expires', utils.get_timestamp(expired)))
+
+        security.append(timestamp)
+
+        #  
+        # headers['Content-Type'] = 'application/soap+xml;charset=UTF-8'
+
+        # security = utils.get_security_header(envelope)
+
+        # created = datetime.utcnow()
+        # expired = created + timedelta(seconds=1 * 60)
+
+        # timestamp = utils.WSU('Timestamp')
+        # timestamp.append(utils.WSU('Created', created.replace(microsecond=0).isoformat()+'Z'))
+        # timestamp.append(utils.WSU('Expires', expired.replace(microsecond=0).isoformat()+'Z'))
+
+        # security.append(timestamp)
+
+        # super().apply(envelope, headers)
+        return envelope, headers
+        
+    # Override response verification and skip response verification for now...
+    # Zeep does not supprt Signature verification with different certificate...
+    # Ref. https://github.com/mvantellingen/python-zeep/pull/822/  "Add support for different signing and verification certificates #822"
     def verify(self, envelope):
         pass
 
@@ -59,7 +99,9 @@ with open(pfx_cert, "rb") as f:
 
     session.auth = HTTPBasicAuth(user, password)
     transport = Transport(session=session)
-    client = Client(wsdl_url, transport=transport, wsse=CustomSignature([signature]))
+    history = HistoryPlugin()
+    client = Client(wsdl_url, transport=transport, wsse=BinarySignatureTimestamp([signature]), plugins=[history])
+    client.settings.raw_response = True
 
     request_data = {
         'codigoInformacion': '1855',
@@ -73,15 +115,15 @@ with open(pfx_cert, "rb") as f:
         'parametrosConsulta': request_data
     }
 
-    import xml.etree.ElementTree as ET
-    from zeep.exceptions import Fault
-
-    response = client.service.consultaXml(**request_parameter)
     try:
+        response = client.service.consultaXml(**request_parameter)
         root = ET.fromstring(response.text)
         error_text = next(root.iter("faultstring")).text
-    except:
-        error_text = "" 
+    except Exception as e:
+        error_text = str(e)
 
     if error_text:
         raise Fault(error_text)
+    
+    for hist in [history.last_sent, history.last_received]:
+        print(etree.tostring(hist["envelope"], encoding="unicode", pretty_print=True))
