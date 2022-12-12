f4dcc3320dc758849dc3e16cf79e78a126062d77
Z <babucse11@gmail.com>
2022-12-12 18:58:26 +0600

    completed

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import os
+import json
 from requests.auth import HTTPBasicAuth
 from requests import Session
-from zeep import xsd, Client, Settings
+from zeep import Client
 from zeep.transports import Transport
 from zeep.wsse.signature import Signature
-from zeep.plugins import HistoryPlugin
-from lxml import etree
 from cryptography.hazmat.primitives.serialization import pkcs12
 from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
 from cryptography.hazmat.backends import default_backend
@@ -14,7 +12,7 @@ from datetime import datetime, timedelta
 from zeep.wsse.utils import WSU
 from zeep.wsse.signature import Signature
 
-wsdl_url = 'InformacionComercialWS.wsdl' #https://miportafoliouat.transunion.co/InformacionComercialWS/services/InformacionComercial?wsdl
+wsdl_url = 'https://miportafoliouat.transunion.co/InformacionComercialWS/services/InformacionComercial?wsdl'
 user = "520825"
 password = "COHd6zaIf*08"
 
@@ -42,25 +40,23 @@ class SignatureTimestamp(Signature):
         security.append(timestamp_token)
 
         super().apply(envelope, headers)
+
         return envelope, headers
 
     def verify(self, envelope):
         pass
 
 with open(pfx_cert, "rb") as f:
+
     private_key, certificate = pkcs12.load_key_and_certificates(f.read(), pfx_password.encode('utf8'), default_backend())[:2]
     
     session = Session()
     session.auth = HTTPBasicAuth(user, password)
     transport = Transport(session=session)
-    history = HistoryPlugin()
 
-    pk = "certs/private.pem"
-    cert = "certs/public.pem"
-    try:
-        os.mkdir("certs")
-    except FileExistsError:
-        pass
+    pk = "private.pem"
+    cert = "public.pem"
+
     with open(cert, "wb") as f:
         f.write(certificate.public_bytes(encoding=Encoding.PEM))
         f.close()
@@ -70,22 +66,14 @@ with open(pfx_cert, "rb") as f:
                                           format=PrivateFormat.TraditionalOpenSSL,
                                           encryption_algorithm=NoEncryption()))
         f.close()
-    
+
     signature = SignatureTimestamp(pk, cert, pfx_password)
-    settings = Settings(strict=False, xml_huge_tree=True)
-    client = Client(wsdl_url, transport=transport, wsse=signature, plugins=[history], settings=settings)    
-    client.set_ns_prefix("inf", "http://infocomercial.cifin.asobancaria.com")
-    client.set_ns_prefix("dto", "http://dto.infocomercial.cifin.asobancaria.com")
-    client.set_ns_prefix("xsd", "http://www.w3.org/2001/XMLSchema")
-    client.set_ns_prefix("xsi", "http://www.w3.org/2001/XMLSchema-instance")
-    client.set_ns_prefix('soapenc', 'http://schemas.xmlsoap.org/soap/encoding/')
-
-    dto_factory = client.type_factory('dto')
-    parameters = dto_factory.ParametrosConsultaDTO(codigoInformacion='1855', motivoConsulta='24', numeroIdentificacion='37685317', tipoIdentificacion='1')
-
-    with client.settings(raw_response=True): 
-        response = client.service.consultaXml(parametrosConsulta = parameters)
-    print (response.text)
-
-    for hist in [history.last_sent, history.last_received]:
-        print(etree.tostring(hist["envelope"], encoding="unicode", pretty_print=False))
+    client = Client(wsdl_url, transport=transport, wsse=signature)
+    
+    request_file = open('input.json')
+    request_data = json.load(request_file)
+    request_file.close()
+    with client.settings(raw_response=True):
+        response = client.service.consultaXml(**request_data)
+        with open("output.xml", "w") as f:
+            f.write(response.text)
